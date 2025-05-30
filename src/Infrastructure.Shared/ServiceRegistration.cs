146b7f289e9b753a2f22a83dfb4cb2f9aaba2d21
Pete Livermore <peterlivermore@gmail.com>
2025-05-30 17:49:10 +0100

    feat(infra-cache): add Redis cache

---
+++ src/Infrastructure.Shared/ServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces.Cache;
+using Infrastructure.Shared.Configuration.Redis;
+using Infrastructure.Shared.Services.Cache;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Shared;
+
+public static class ServiceRegistration
+{
+    public static void AddSharedInfrastructure(
+        this IServiceCollection services,
+        IConfiguration config
+    )
+    {
+        var redisConfig = config.GetSection(RedisOptions.Redis).Get<RedisOptions>();
+        if (redisConfig is null)
+        {
+            throw new InvalidOperationException(
+                $"{RedisOptions.Redis} config is missing or incomplete in appsettings.json."
+            );
+        }
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = redisConfig.Configuration;
+            options.InstanceName = redisConfig.InstanceName;
+        });
+
+        services.AddScoped<ICacheService, CacheService>();
+    }
+}
