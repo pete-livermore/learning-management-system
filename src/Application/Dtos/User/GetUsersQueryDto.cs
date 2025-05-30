90395e2675fd488dc4e6fe8344129f2530f65f01
Pete Livermore <peterlivermore@gmail.com>
2025-05-30 17:32:43 +0100

    feat(user-dtos): add DTO for GET query params

---
+++ src/Application/Dtos/User/GetUsersQueryDto.cs
@@ -0,0 +1,19 @@
+namespace Application.Dtos.User;
+
+public record GetUsersQueryDto
+{
+    public record PopulateDto { }
+
+    public record FiltersDto
+    {
+        public string? FirstName { get; init; } = null;
+        public string? LastName { get; init; } = null;
+        public string? Email { get; init; } = null;
+    }
+
+    public FiltersDto Filters { get; init; } = new();
+    public PopulateDto Populate { get; init; } = new();
+
+    public int? Pages { get; init; }
+    public int? Start { get; init; } = null;
+}
