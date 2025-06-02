be5148f649e25cd41beb484a1e4942555745be46
Pete Livermore <peterlivermore@gmail.com>
2025-06-02 16:53:02 +0100

    feat: add data caching to getUsers query

--- src/Application/UseCases/Users/Queries/GetUsersQuery.cs
+++ src/Application/UseCases/Users/Queries/GetUsersQuery.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using Application.Common.Dtos;
+using Application.Common.Interfaces.Cache;
 using Application.Common.Interfaces.Repositories;
 using Application.UseCases.Users.Dtos;
 using Application.Wrappers.Results;
@@ -15,10 +17,12 @@ public record class GetUsersQuery : IRequest<Result<PaginatedList<UserDto>>>
 public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, Result<PaginatedList<UserDto>>>
 {
     private readonly IUsersRepository _usersRepository;
+    private readonly ICacheService _cacheService;
 
-    public GetUsersQueryHandler(IUsersRepository usersRepository)
+    public GetUsersQueryHandler(IUsersRepository usersRepository, ICacheService cacheService)
     {
         _usersRepository = usersRepository;
+        _cacheService = cacheService;
     }
 
     public async Task<Result<PaginatedList<UserDto>>> Handle(
@@ -28,6 +32,14 @@ public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, Result<Pagina
     {
         var filters = query.Filters;
         var pagination = query.Pagination;
+        var filtersString = JsonSerializer.Serialize(filters);
+        var cacheKey = $"Users_{filtersString}";
+        var cachedUsersList = await _cacheService.GetValueAsync<PaginatedList<UserDto>>(cacheKey);
+
+        if (cachedUsersList is not null)
+        {
+            return Result<PaginatedList<UserDto>>.Success(cachedUsersList);
+        }
 
         var (users, totalPages) = await _usersRepository.FindMany(filters, pagination);
         var userDtos = users
@@ -43,6 +55,8 @@ public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, Result<Pagina
 
         int pageIndex = pagination?.PageIndex ?? 1;
         var usersList = new PaginatedList<UserDto>(userDtos, pageIndex, totalPages);
+
+        await _cacheService.SetValueAsync(cacheKey, usersList);
 
         return Result<PaginatedList<UserDto>>.Success(usersList);
     }
