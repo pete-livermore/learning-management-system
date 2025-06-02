d88e279e75106d3072018a0784928e5dc35a50f0
Pete Livermore <peterlivermore@gmail.com>
2025-06-02 15:32:59 +0100

    feat: add new method to configure options

--- src/WebApi/Extensions/ServiceExtensions.cs
+++ src/WebApi/Extensions/ServiceExtensions.cs
@@ -1,9 +1,19 @@
+using LearningManagementSystem.Infrastructure.Identity.Configuration;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Extensions;
 
 public static class ServiceExtensions
 {
+    public static void AddConfigOptions(this IServiceCollection services, IConfiguration config)
+    {
+        services
+            .AddOptions<SecurityOptions>()
+            .Bind(config.GetSection(SecurityOptions.Security))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+    }
+
     public static void AddApiVersioningExtension(this IServiceCollection services)
     {
         services.AddApiVersioning(options =>
