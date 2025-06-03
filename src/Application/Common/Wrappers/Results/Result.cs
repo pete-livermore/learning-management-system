ffe8dce2253b8259370f178502d3c9847ffc7ba7
Pete Livermore <peterlivermore@gmail.com>
2025-06-03 18:18:23 +0100

    refactor: pluralise error field

--- src/Application/Common/Wrappers/Results/Result.cs
+++ src/Application/Common/Wrappers/Results/Result.cs
@@ -5,43 +5,39 @@ namespace Application.Wrappers.Results;
 public class Result
 {
     public bool IsSuccess { get; }
-    public Error Error { get; }
+    public IReadOnlyList<Error> Errors { get; }
 
     public bool IsFailure => !IsSuccess;
 
-    public Result(bool isSuccess, Error error)
+    public Result(bool isSuccess, params Error[] errors)
     {
-        if (isSuccess && error != Error.None || !isSuccess && error == Error.None)
+        var hasErrors = errors.Any(err => err != Error.None);
+        if (isSuccess && hasErrors || !isSuccess && !hasErrors)
         {
-            throw new ArgumentException("Invalid error", nameof(error));
+            throw new ArgumentException("Invalid errors", nameof(errors));
         }
 
         IsSuccess = isSuccess;
 
-        Error = error;
+        Errors = errors;
     }
 
     public static Result Success() => new(true, Error.None);
 
-    public static Result Failure(Error error) => new(false, error);
+    public static Result Failure(params Error[] errors) => new(false, errors);
 }
 
 public class Result<T> : Result
 {
     public T Value { get; }
 
-    public Result(bool isSuccess, T value, Error error)
-        : base(isSuccess, error)
+    public Result(bool isSuccess, T value, params Error[] errors)
+        : base(isSuccess, errors)
     {
-        if (isSuccess && error != Error.None || !isSuccess && error == Error.None)
-        {
-            throw new ArgumentException("Invalid error", nameof(error));
-        }
-
         Value = value;
     }
 
     public static Result<T> Success(T data) => new(true, data, Error.None);
 
-    public static new Result<T> Failure(Error error) => new(false, default!, error);
+    public static new Result<T> Failure(params Error[] errors) => new(false, default!, errors);
 }
