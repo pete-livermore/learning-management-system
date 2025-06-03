023fe5946a2c1aba0dc0d330760dfa5b54715a08
Pete Livermore <peterlivermore@gmail.com>
2025-06-03 18:27:48 +0100

    feat: add error handling logic

---
+++ src/WebApi/Controllers/v1/ApiController.cs
@@ -0,0 +1,47 @@
+using Application.Common.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace WebApi.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        protected ActionResult Problem(IReadOnlyList<Error> errors)
+        {
+            if (errors.Any(err => err.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+
+            return Problem(errors[0]);
+        }
+
+        private ObjectResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
+            {
+                ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+                ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError,
+            };
+
+            return Problem(statusCode: statusCode, title: error.Description);
+        }
+
+        private ActionResult ValidationProblem(IReadOnlyList<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+    }
+}
