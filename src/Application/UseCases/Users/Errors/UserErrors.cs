157cab132b87ea1b836577876cd7291cf550a026
Pete Livermore <peterlivermore@gmail.com>
2025-06-03 18:28:16 +0100

    refactor: add error type to error constructors

--- src/Application/UseCases/Users/Errors/UserErrors.cs
+++ src/Application/UseCases/Users/Errors/UserErrors.cs
@@ -5,20 +5,20 @@ namespace Application.UseCases.Users.Errors
     public static class UserErrors
     {
         private const string ErrorContext = "Users";
-
         public const string NotFoundCode = $"{ErrorContext}.NotFound";
         public const string ConflictCode = $"{ErrorContext}.Conflict";
         public const string UnexpectedCode = $"{ErrorContext}.Unexpected";
 
         public static Error NotFound(int id) =>
-            new(NotFoundCode, $"The user with Id '{id}' was not found");
+            new(ErrorType.NotFound, NotFoundCode, $"The user with Id '{id}' was not found");
 
         public static Error NotFound(string email) =>
-            new(NotFoundCode, $"The user with email '{email}' was not found");
+            new(ErrorType.NotFound, NotFoundCode, $"The user with email '{email}' was not found");
 
         public static Error Conflict(string email) =>
-            new(ConflictCode, $"The user with email '{email}' already exists");
+            new(ErrorType.Conflict, ConflictCode, $"The user with email '{email}' already exists");
 
-        public static Error Unexpected(string message) => new(UnexpectedCode, message);
+        public static Error Unexpected(string message) =>
+            new(ErrorType.Unexpected, UnexpectedCode, message);
     }
 }
