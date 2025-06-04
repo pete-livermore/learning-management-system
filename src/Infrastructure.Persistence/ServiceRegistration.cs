7ea1fb606709d4196a61b52542e3ad56e1ad2835
Pete Livermore <peterlivermore@gmail.com>
2025-06-04 18:05:35 +0100

    feat: register files repo

--- src/Infrastructure.Persistence/ServiceRegistration.cs
+++ src/Infrastructure.Persistence/ServiceRegistration.cs
@@ -19,5 +19,6 @@ public static class ServiceRegistration
         );
 
         services.AddScoped<IUsersRepository, UsersRepository>();
+        services.AddScoped<IFilesRepository, FilesRepository>();
     }
 }
