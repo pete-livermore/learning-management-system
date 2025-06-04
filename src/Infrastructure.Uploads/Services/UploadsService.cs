d93bf6362a6926f3b2c28412248426a600071350
Pete Livermore <peterlivermore@gmail.com>
2025-06-04 15:52:43 +0100

    feat: add uploads service implementation

---
+++ src/Infrastructure.Uploads/Services/UploadsService.cs
@@ -0,0 +1,47 @@
+using Application.UseCases.Uploads.Dtos;
+using Application.UseCases.Uploads.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Infrastructure.Uploads.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Uploads.Services
+{
+    public class UploadsService : IUploadsService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public UploadsService(IOptions<CloudinaryOptions> cloudinaryConfig)
+        {
+            _cloudinary = new Cloudinary(cloudinaryConfig.Value.Url);
+        }
+
+        public async Task<UploadedFileDto> Upload(
+            IFormFile file,
+            FileMetadataDto? fileMetadata = null
+        )
+        {
+            await using var stream = file.OpenReadStream();
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, stream),
+                UseFilename = true,
+                UniqueFilename = false,
+                Overwrite = true,
+            };
+            var cloudinaryUploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+            return new UploadedFileDto()
+            {
+                Url = cloudinaryUploadResult.Url.ToString(),
+                ResourceType = cloudinaryUploadResult.ResourceType,
+                ProviderMetadata = new UploadProviderMetadata()
+                {
+                    Id = cloudinaryUploadResult.PublicId,
+                },
+            };
+        }
+    }
+}
