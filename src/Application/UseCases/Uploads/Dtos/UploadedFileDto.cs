f50b49d3a85b2040fc8b983c2f06cfb8600eef58
Pete Livermore <peterlivermore@gmail.com>
2025-06-04 15:52:01 +0100

    feat: add uploaded file DTO

---
+++ src/Application/UseCases/Uploads/Dtos/UploadedFileDto.cs
@@ -0,0 +1,15 @@
+namespace Application.UseCases.Uploads.Dtos
+{
+    public class UploadProviderMetadata
+    {
+        public required string Id { get; set; }
+    }
+
+    public record UploadedFileDto
+    {
+        public required string Url { get; set; }
+
+        public required string ResourceType { get; set; }
+        public required UploadProviderMetadata ProviderMetadata { get; set; }
+    }
+}
