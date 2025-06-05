d6dca7057e36a6cf2a277e21e91d36efc7b461e0
Pete Livermore <peterlivermore@gmail.com>
2025-06-05 14:09:58 +0100

    refactor: change to use generic entity owner DTO

--- src/Application/UseCases/Uploads/Dtos/FileDto.cs
+++ src/Application/UseCases/Uploads/Dtos/FileDto.cs
@@ -1,10 +1,7 @@
+using Application.Common.Dtos;
+
 namespace Application.UseCases.Uploads.Dtos
 {
-    public class FileOwnerDto
-    {
-        public int Id { get; init; }
-    }
-
     public record FileDto
     {
         public int Id { get; init; }
@@ -12,8 +9,7 @@ namespace Application.UseCases.Uploads.Dtos
         public required string Mime { get; set; }
         public required string Ext { get; set; }
         public required string ProviderId { get; set; }
-        public required string ResourceType { get; set; }
-        public required FileOwnerDto Owner { get; set; }
+        public required EntityOwnerDto Owner { get; set; }
         public required DateTime CreatedAt { get; set; }
         public required DateTime UpdatedAt { get; set; }
     }
