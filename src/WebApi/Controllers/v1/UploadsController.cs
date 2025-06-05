a38f055c8ed89e8d9832f92eed7ba2275a79c270
Pete Livermore <peterlivermore@gmail.com>
2025-06-05 14:13:16 +0100

    feat: add uploads controller

---
+++ src/WebApi/Controllers/v1/UploadsController.cs
@@ -0,0 +1,45 @@
+using Application.UseCases.Uploads.Commands;
+using Application.UseCases.Uploads.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.v1;
+
+namespace WebApi.Controllers.V1
+{
+    [Authorize]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/[controller]")]
+    public class UploadsController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public UploadsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(
+            IFormFile file,
+            [FromForm] FileMetadataDto fileMetadata
+        )
+        {
+            var uploadResult = await _mediator.Send(
+                new CreateFileCommand() { FileContent = file, FileMetadata = fileMetadata }
+            );
+
+            if (uploadResult.IsFailure)
+            {
+                return Problem(uploadResult.Errors);
+            }
+
+            return CreatedAtAction(
+                nameof(UploadFile),
+                new { id = uploadResult.Value.Id },
+                uploadResult.Value
+            );
+        }
+    }
+}
