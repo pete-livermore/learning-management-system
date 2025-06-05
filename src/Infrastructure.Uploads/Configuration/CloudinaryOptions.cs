4be73c2c4c0861e33ad6924870ec77ff2bcc3cbf
Pete Livermore <peterlivermore@gmail.com>
2025-06-05 14:11:37 +0100

    refactor: make url property required

--- src/Infrastructure.Uploads/Configuration/CloudinaryOptions.cs
+++ src/Infrastructure.Uploads/Configuration/CloudinaryOptions.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Infrastructure.Uploads.Configuration
 {
     public sealed class CloudinaryOptions
     {
         public const string Cloudinary = "Cloudinary";
 
+        [Required]
         public required string Url { get; set; }
     }
 }
