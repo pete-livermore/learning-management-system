67b2dd6a95567f09a68dbe0aa10f8843a675568a
Pete Livermore <peterlivermore@gmail.com>
2025-06-12 21:48:40 +0100

    feat: add ApplicationUserDto

---
+++ src/Application/UseCases/Security/Dtos/ApplicationUserDto.cs
@@ -0,0 +1,8 @@
+namespace Application.UseCases.Users.Dtos;
+
+public record ApplicationUserDto
+{
+    public required Guid Id { get; init; }
+    public required string Email { get; init; }
+    public required List<string> Roles { get; init; }
+}
