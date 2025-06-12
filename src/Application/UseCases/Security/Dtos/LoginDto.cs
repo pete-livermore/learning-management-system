16abed737caf52f8c6c230f1c2a782629543f319
Pete Livermore <peterlivermore@gmail.com>
2025-06-12 21:48:25 +0100

    refactor: remove annotations

--- src/Application/UseCases/Security/Dtos/LoginDto.cs
+++ src/Application/UseCases/Security/Dtos/LoginDto.cs
@@ -1,15 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Application.UseCases.Security.Dtos
 {
     public record LoginDto
     {
-        [Required]
-        [EmailAddress]
         public required string Email { get; init; }
-
-        [Required]
-        [DataType(DataType.Password)]
         public required string Password { get; init; }
     }
 }
