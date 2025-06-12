ca62f96c4028da08f2d659a641294c901bbc7a0f
Pete Livermore <peterlivermore@gmail.com>
2025-06-12 21:38:59 +0100

    refactor: consolidate conceptually similar methods

--- src/Infrastructure.Identity/Extensions/PrincipalExtensions.cs
+++ src/Infrastructure.Identity/Extensions/PrincipalExtensions.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using Domain.Enums;
 
 namespace Infrastructure.Identity.Extensions
 {
@@ -7,43 +8,32 @@ namespace Infrastructure.Identity.Extensions
     /// </summary>
     public static class PrincipalExtensions
     {
-        /// <summary>
-        /// Returns the User Name claim value if present otherwise returns null.
-        /// </summary>
-        /// <param name="principal">The <see cref="ClaimsPrincipal"/> instance this method extends.</param>
-        /// <returns>The Name claim value, or null if the claim is not present.</returns>
-        /// <remarks>The name claim is identified by <see cref="ClaimsIdentity.DefaultNameClaimType"/>.</remarks>
-        public static string? GetUserName(this ClaimsPrincipal principal)
+        public static string? GetClaimValue(this ClaimsPrincipal principal, string claimType)
         {
             if (principal == null)
             {
                 throw new ArgumentNullException(nameof(principal));
             }
-            return principal.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
+            return principal.FindFirstValue(claimType);
         }
 
-        /// <summary>
-        /// Returns the User ID claim value if present otherwise returns null.
-        /// </summary>
-        /// <param name="principal">The <see cref="ClaimsPrincipal"/> instance this method extends.</param>
-        /// <returns>The User ID claim value, or null if the claim is not present.</returns>
-        /// <remarks>The name claim is identified by <see cref="ClaimTypes.NameIdentifier"/>.</remarks>
-        public static string? GetUserId(this ClaimsPrincipal principal)
+        public static IReadOnlyList<UserRole> GetUserRoles(this ClaimsPrincipal principal)
         {
-            if (principal == null)
-            {
-                throw new ArgumentNullException(nameof(principal));
-            }
-            return principal.FindFirstValue(ClaimTypes.NameIdentifier);
-        }
+            List<UserRole> domainRoles = new();
+            List<string> identityRoles = principal
+                .FindAll(ClaimTypes.Role)
+                .Select(c => c.Value)
+                .ToList();
 
-        public static string? GetUserRole(this ClaimsPrincipal principal)
-        {
-            if (principal == null)
+            foreach (var role in identityRoles)
             {
-                throw new ArgumentNullException(nameof(principal));
+                if (Enum.TryParse<UserRole>(role, out var result))
+                {
+                    domainRoles.Add(result);
+                }
+                ;
             }
-            return principal.FindFirstValue(ClaimTypes.Role);
+            return domainRoles.AsReadOnly();
         }
     }
 }
