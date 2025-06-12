f1fdda07aa28161af4ea5d0b0f94da90b1ee2400
Pete Livermore <peterlivermore@gmail.com>
2025-06-12 21:46:15 +0100

    feat: extend from identity DB context

--- src/Infrastructure.Persistence/Contexts/LearningManagementSystemDbContext.cs
+++ src/Infrastructure.Persistence/Contexts/LearningManagementSystemDbContext.cs
@@ -1,22 +1,26 @@
 using Domain.Entities;
+using Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence.Contexts
 {
-    public class LearningManagementSystemDbContext : DbContext
+    public class LearningManagementSystemDbContext
+        : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
     {
         public LearningManagementSystemDbContext(
             DbContextOptions<LearningManagementSystemDbContext> options
         )
             : base(options) { }
 
-        public required DbSet<User> Users { get; set; }
+        public required DbSet<User> DomainUsers { get; set; }
         public required DbSet<UploadFile> Files { get; set; }
         public required DbSet<Lesson> Lessons { get; set; }
         public required DbSet<LessonContent> LessonContents { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(
                 typeof(LearningManagementSystemDbContext).Assembly
             );
