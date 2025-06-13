45b60ec58f6258742ff60345ce84712ae5a0206c
Pete Livermore <peterlivermore@gmail.com>
2025-06-13 12:01:15 +0100

    fix: use correct errors

--- tests/Application.UnitTests/UseCases/Users/Queries/GetUserByIdQueryTests.cs
+++ tests/Application.UnitTests/UseCases/Users/Queries/GetUserByIdQueryTests.cs
@@ -1,4 +1,5 @@
 using Application.Common.Errors;
+using Application.Common.Errors.Factories;
 using Application.Common.Interfaces.Repositories;
 using Application.UseCases.Security.Dtos;
 using Application.UseCases.Security.Interfaces;
@@ -12,32 +13,49 @@ namespace Application.UnitTests.UseCases.Users.Queries
     public class GetUserByIdQueryTests
     {
         private readonly Mock<IUsersRepository> _repositoryMock;
-        private readonly Mock<IUserAccessor> _userAccessorMock;
+        private readonly Mock<ICurrentUserProvider> _currentUserProviderMock;
 
         public GetUserByIdQueryTests()
         {
             _repositoryMock = new Mock<IUsersRepository>();
-            _userAccessorMock = new Mock<IUserAccessor>();
+            _currentUserProviderMock = new Mock<ICurrentUserProvider>();
         }
 
         private GetUserByIdQueryHandler CreateHandler()
         {
-            return new GetUserByIdQueryHandler(_repositoryMock.Object, _userAccessorMock.Object);
+            return new GetUserByIdQueryHandler(
+                _currentUserProviderMock.Object,
+                _repositoryMock.Object
+            );
         }
 
         [Fact]
         public async void ShouldReturnForbidden_WhenUserIsNotAuthorised()
         {
             var handler = CreateHandler();
-            var authenticatedUser = new AuthenticatedUserDto() { Id = 346, Role = UserRole.User };
+            var currentUserDto = new CurrentUserDto()
+            {
+                Id = Guid.NewGuid(),
+                Roles = [UserRole.Learner],
+            };
+            var domainUser = new User()
+            {
+                Id = 267,
+                ApplicationUserId = Guid.NewGuid(),
+                Email = "terest@ghghkl.com",
+                FirstName = "First",
+                LastName = "Last",
+                Role = UserRole.Learner,
+            };
+            _currentUserProviderMock.Setup(cup => cup.GetCurrentUser()).Returns(currentUserDto);
+            _repositoryMock.Setup(rp => rp.FindByIdAsync(It.IsAny<int>())).ReturnsAsync(domainUser);
             var getUserByIdQuery = new GetUserByIdQuery() { UserId = 12 };
-
-            _userAccessorMock.Setup(ua => ua.GetCurrentUser()).Returns(authenticatedUser);
 
             var result = await handler.Handle(getUserByIdQuery, new CancellationToken());
 
             Assert.True(result.IsFailure);
-            Assert.Equal(ErrorType.Forbidden, result.Errors[0].Type);
+            Assert.Equal(ErrorType.Security, result.Errors[0].Type);
+            Assert.Equal(SecurityError.ForbiddenCode, result.Errors[0].Code);
         }
 
         [Fact]
@@ -45,17 +63,23 @@ namespace Application.UnitTests.UseCases.Users.Queries
         {
             var handler = CreateHandler();
             var userIdToQuery = 78;
-            var authenticatedUser = new AuthenticatedUserDto() { Id = 200, Role = UserRole.Admin };
+            var currentUserDto = new CurrentUserDto()
+            {
+                Id = Guid.NewGuid(),
+                Roles = [UserRole.Administrator],
+            };
+            _currentUserProviderMock.Setup(ua => ua.GetCurrentUser()).Returns(currentUserDto);
+
             var getUserByIdQuery = new GetUserByIdQuery() { UserId = userIdToQuery };
 
-            _userAccessorMock.Setup(ua => ua.GetCurrentUser()).Returns(authenticatedUser);
-            _repositoryMock.Setup(ur => ur.FindById(userIdToQuery)).ReturnsAsync((User?)null);
+            _repositoryMock.Setup(ur => ur.FindByIdAsync(userIdToQuery)).ReturnsAsync((User?)null);
 
             var result = await handler.Handle(getUserByIdQuery, new CancellationToken());
 
-            _repositoryMock.Verify(r => r.FindById(userIdToQuery), Times.Once);
+            _repositoryMock.Verify(r => r.FindByIdAsync(userIdToQuery), Times.Once);
             Assert.True(result.IsFailure);
-            Assert.Equal(ErrorType.NotFound, result.Errors[0].Type);
+            Assert.Equal(ErrorType.ResourcePersistence, result.Errors[0].Type);
+            Assert.Equal(ResourceError.NotFoundCode, result.Errors[0].Code);
         }
 
         [Fact]
@@ -63,31 +87,33 @@ namespace Application.UnitTests.UseCases.Users.Queries
         {
             var handler = CreateHandler();
             var userIdToQuery = 78;
-            var authenticatedUser = new AuthenticatedUserDto()
+            var applicationUserId = Guid.NewGuid();
+            var currentUserDto = new CurrentUserDto()
             {
-                Id = userIdToQuery,
-                Role = UserRole.User,
+                Id = applicationUserId,
+                Roles = [UserRole.Learner],
             };
+            _currentUserProviderMock.Setup(ua => ua.GetCurrentUser()).Returns(currentUserDto);
+
             var getUserByIdQuery = new GetUserByIdQuery() { UserId = userIdToQuery };
 
-            _userAccessorMock.Setup(ua => ua.GetCurrentUser()).Returns(authenticatedUser);
             _repositoryMock
-                .Setup(ur => ur.FindById(userIdToQuery))
+                .Setup(ur => ur.FindByIdAsync(userIdToQuery))
                 .ReturnsAsync(
                     new User()
                     {
                         Id = userIdToQuery,
                         FirstName = "test",
                         LastName = "person",
-                        Password = "P@ssw0rd",
+                        ApplicationUserId = applicationUserId,
                         Email = "test.person@email.com",
-                        Role = UserRole.User,
+                        Role = UserRole.Learner,
                     }
                 );
 
             var result = await handler.Handle(getUserByIdQuery, new CancellationToken());
 
-            _repositoryMock.Verify(r => r.FindById(userIdToQuery), Times.Once);
+            _repositoryMock.Verify(r => r.FindByIdAsync(userIdToQuery), Times.Once);
             Assert.True(result.IsSuccess);
         }
     }
