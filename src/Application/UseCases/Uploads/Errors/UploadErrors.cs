29acc3f192c9eba735b2990056a76189e18084d5
Pete Livermore <peterlivermore@gmail.com>
2025-06-13 11:54:08 +0100

    refactor: remove redundant use case specific errors

--- src/Application/UseCases/Uploads/Errors/UploadErrors.cs
+++
@@ -1,9 +0,0 @@
-using Application.Common.Errors;
-
-namespace Application.UseCases.Uploads.Errors;
-
-public static class UploadErrors
-{
-    public static Error Validation(string message) =>
-        new(ErrorType.Validation, "Uploads.Validation", message);
-}
