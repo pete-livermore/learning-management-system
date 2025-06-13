f12243f71414abae83f4a916bb1a10459d7110a0
Pete Livermore <peterlivermore@gmail.com>
2025-06-13 11:54:45 +0100

    refactor: use class to wrap factory errors

--- src/Application/UseCases/Users/Errors/UserErrors.cs
+++ src/Application/UseCases/Users/Errors/UserErrors.cs
@@ -1,24 +1,16 @@
+using System;
 using Application.Common.Errors;
+using Application.Common.Errors.Factories;
 
-namespace Application.UseCases.Users.Errors
+namespace Application.UseCases.Users.Errors;
+
+public static class UserErrors
 {
-    public static class UserErrors
-    {
-        private const string ErrorContext = "Users";
-        public const string NotFoundCode = $"{ErrorContext}.NotFound";
-        public const string ConflictCode = $"{ErrorContext}.Conflict";
-        public const string UnexpectedCode = $"{ErrorContext}.Unexpected";
+    public static Error NotFound(int userId) =>
+        ResourceError.NotFound($"The user with {userId} was not found");
 
-        public static Error NotFound(int id) =>
-            new(ErrorType.NotFound, NotFoundCode, $"The user with Id '{id}' was not found");
-
-        public static Error NotFound(string email) =>
-            new(ErrorType.NotFound, NotFoundCode, $"The user with email '{email}' was not found");
-
-        public static Error Conflict(string email) =>
-            new(ErrorType.Conflict, ConflictCode, $"The user with email '{email}' already exists");
-
-        public static Error Unexpected(string message) =>
-            new(ErrorType.Unexpected, UnexpectedCode, message);
-    }
+    public static Error Forbidden() =>
+        SecurityError.Forbidden(
+            "The current application user is forbidden from accessing this user resource"
+        );
 }
