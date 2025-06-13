033d5e68459bcb2734972f8802c3ba422aab30c9
Pete Livermore <peterlivermore@gmail.com>
2025-06-13 09:41:58 +0100

    feat: add reference to identity user

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -8,7 +8,7 @@ namespace Domain.Entities
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
-        public required string Password { get; set; }
         public required UserRole Role { get; set; }
+        public required Guid ApplicationUserId { get; init; }
     }
 }
