f2c4e10f777ef4e0d9668add0756cf4e0567a84a
Pete Livermore <peterlivermore@gmail.com>
2025-06-13 09:48:10 +0100

    refactor: remove data annotations

--- src/Application/UseCases/Users/Dtos/ReplaceUserDto.cs
+++ src/Application/UseCases/Users/Dtos/ReplaceUserDto.cs
@@ -1,23 +1,10 @@
 namespace Application.UseCases.Users.Dtos
 {
-    using System.ComponentModel.DataAnnotations;
-
     public record ReplaceUserDto
     {
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
         public required string FirstName { get; init; }
-
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
         public required string LastName { get; init; }
-
-        [Required]
-        [EmailAddress]
         public required string Email { get; init; }
-
-        [Required]
-        [DataType(DataType.Password)]
         public required string Password { get; init; }
     }
 }
