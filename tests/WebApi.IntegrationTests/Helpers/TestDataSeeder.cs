cd4851d5c16d4a6b0e0f926afdb884268e1fefa9
Pete Livermore <peterlivermore@gmail.com>
2025-06-16 19:01:56 +0100

    refactor: inject identity service

--- tests/WebApi.IntegrationTests/Helpers/TestDataSeeder.cs
+++ tests/WebApi.IntegrationTests/Helpers/TestDataSeeder.cs
@@ -1,40 +1,70 @@
+using System.Text.Json;
+using Application.Common.Interfaces.Security;
+using Application.UseCases.Security.Dtos;
 using Application.UseCases.Users.Commands;
 using Application.UseCases.Users.Dtos;
 using Domain.Enums;
 using MediatR;
+using WebApi.IntegrationTests.Fixtures;
 
 namespace WebApi.IntegrationTests.Helpers;
 
 public class TestDataSeeder
 {
+    private readonly ILogger<TestDataSeeder> _logger;
+    private readonly IIdentityService _identityService;
     private readonly IMediator _mediator;
 
-    public TestDataSeeder(IMediator mediator)
+    public TestDataSeeder(
+        IMediator mediator,
+        IIdentityService identityService,
+        ILogger<TestDataSeeder> logger
+    )
     {
         _mediator = mediator;
+        _identityService = identityService;
+        _logger = logger;
     }
 
-    private async Task SeedUsers()
+    public async Task SeedRoles()
     {
+        var role = new CreateApplicationRoleDto() { Name = UserRole.Administrator.ToString() };
+        var createResult = await _identityService.CreateRoleAsync(role);
+
+        if (createResult.IsFailure)
+        {
+            throw new InvalidOperationException(
+                $"Seeding roles failed: {createResult.Errors[0].Description}"
+            );
+        }
+    }
+
+    public async Task SeedUsers()
+    {
+        var testUserDto = TestUserFixture.Admin;
+        Console.WriteLine($"USER TO SEED => {JsonSerializer.Serialize(testUserDto)}");
+        var userEmail = testUserDto.Email;
+        var existingUser = await _identityService.FindUserByEmailAsync(userEmail);
+
+        if (existingUser is not null)
+        {
+            _logger.LogInformation("Application user already exists");
+        }
+
         var createUserDto = new CreateUserDto()
         {
-            FirstName = "John",
-            LastName = "Smith",
-            Email = "john_smith@email.com",
-            Role = UserRole.User.ToString(),
-            Password = "password",
+            Email = testUserDto.Email,
+            FirstName = testUserDto.FirstName,
+            LastName = testUserDto.LastName,
+            Password = testUserDto.Password,
+            Role = testUserDto.Role.ToString(),
         };
         var createUserCommand = new CreateUserCommand() { CreateCommand = createUserDto };
         var result = await _mediator.Send(createUserCommand);
 
         if (result.IsFailure)
         {
-            throw new Exception("User seeding failed");
+            throw new Exception($"User seeding failed: {result.Errors[0].Description}");
         }
-    }
-
-    public async Task SeedData()
-    {
-        await SeedUsers();
     }
 }
