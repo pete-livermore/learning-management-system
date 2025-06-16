c1f4a99a0ed91ec0a5b4270aec47a07c09261838
Pete Livermore <peterlivermore@gmail.com>
2025-06-16 18:46:56 +0100

    refactor: make method names more explicit

--- src/Application/Common/Interfaces/Security/IIdentityService.cs
+++ src/Application/Common/Interfaces/Security/IIdentityService.cs
@@ -7,9 +7,17 @@ namespace Application.Common.Interfaces.Security
     public interface IIdentityService
     {
         Task<Result<ApplicationUserDto>> CreateUserAsync(CreateApplicationUserDto createUserDto);
+        Task<Result<ApplicationUserDto>> FindUserByIdAsync(Guid userId);
+        Task<Result<ApplicationUserDto>> FindUserByEmailAsync(string userEmail);
+        Task<Result> UpdateUserAsync(
+            Guid userId,
+            UpdateApplicationUserDto updateApplicationUserDto
+        );
+        Task<Result> DeleteUserAsync(Guid userId);
+        Task<Result<ApplicationRoleDto>> CreateRoleAsync(
+            CreateApplicationRoleDto createApplicationRoleDto
+        );
         Task<Result<List<string>>> GetUserRolesAsync(Guid userId);
         Task<Result<string>> AuthenticateUserAsync(string userEmail, string password);
-        Task<Result> UpdateUser(Guid userId, UpdateApplicationUserDto updateApplicationUserDto);
-        Task<Result> DeleteAsync(Guid userId);
     }
 }
