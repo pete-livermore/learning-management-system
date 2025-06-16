7cc3f38006c7faee0488413b1df716151ec9a65f
Pete Livermore <peterlivermore@gmail.com>
2025-06-16 18:59:30 +0100

    feat: add user faker class

---
+++ tests/Application.UnitTests/Fakes/UserFaker.cs
@@ -0,0 +1,14 @@
+using Bogus;
+using Domain.Entities;
+
+namespace Application.UnitTests.Fakes;
+
+public static class UserFaker
+{
+    public static User Create() =>
+        new Faker<User>()
+            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+            .RuleFor(u => u.LastName, f => f.Name.LastName())
+            .RuleFor(u => u.Email, f => f.Internet.Email())
+            .Generate();
+}
