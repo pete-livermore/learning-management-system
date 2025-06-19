95258da4e983072067b8ce5357e2c3810cea3772
Pete Livermore <peterlivermore@gmail.com>
2025-06-19 11:09:57 +0100

    refactor: remove redundant property

--- tests/WebApi.IntegrationTests/Fixtures/TestUserFixture.cs
+++ tests/WebApi.IntegrationTests/Fixtures/TestUserFixture.cs
@@ -1,16 +1,15 @@
 using Domain.Enums;
+using Domain.Users.Enums;
 
 namespace WebApi.IntegrationTests.Fixtures;
 
 public sealed record TestUserDto
 {
-    public int Id { get; init; } = 1;
     public string FirstName { get; init; } = "Foo";
     public string LastName { get; init; } = "Bar";
     public string Email { get; init; } = "foo_bar@email.com";
     public string Password { get; init; } = "Secur3.P@ssw0rd";
     public required UserRole Role { get; init; }
-    public required Guid ApplicationUserId { get; init; }
 }
 
 public static class TestUserFixture
@@ -19,11 +18,7 @@ public static class TestUserFixture
 
     static TestUserFixture()
     {
-        _adminUser = new TestUserDto()
-        {
-            Role = UserRole.Administrator,
-            ApplicationUserId = Guid.NewGuid(),
-        };
+        _adminUser = new TestUserDto() { Role = UserRole.Administrator };
     }
 
     public static TestUserDto Admin => _adminUser;
