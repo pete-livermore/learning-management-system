05ef5f3931689ab0152c3186337b39b6d34b5c51
Pete Livermore <peterlivermore@gmail.com>
2025-06-25 19:45:19 +0100

    feat: add xml documentation

--- src/Application/Security/Interfaces/ICurrentUserProvider.cs
+++ src/Application/Security/Interfaces/ICurrentUserProvider.cs
@@ -4,5 +4,11 @@ namespace Application.Security.Interfaces;
 
 public interface ICurrentUserProvider
 {
+    /// <summary>
+    /// Provides the current authenticated user based on the JWT identity claim
+    /// </summary>
+    /// <returns>
+    /// A <see cref="CurrentUserDto"/> containing the authenticated user data
+    /// </returns>
     CurrentUserDto GetCurrentUser();
 }
